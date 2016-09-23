de5672d2db907a18dad6747f628fb3ddf50662e0
rimaan19 <m.berg@tu-bs.de>
2016-09-23 19:59:09 +0200

    Add files via upload
    Some cleanup.

--- projectTriangle.hs
+++ projectTriangle.hs
@@ -1,20 +1,27 @@
 import qualified Data.Vector as V
 
-data Point    = Point{getX :: Double, getY :: Double, getZ :: Double} deriving(Show, Eq)
-data Triangle = Triangle{getPoint0 :: Point, getPoint1 :: Point, getPoint2 :: Point} deriving(Show, Eq)
- 
+data Point    = Point{ getX :: !Double
+                     , getY :: !Double
+                     , getZ :: !Double
+                     } deriving(Show, Eq)
+
 type Colour   = Double
-data Pixel    = Pixel{getXp :: Double, getYp :: Double, getColour :: Colour} deriving(Show, Eq)
+data Vertex   = Vertex{ getPoint  :: !Point
+                      , getColour :: !Colour
+                      } deriving(Show, Eq)
+
+data Triangle = Triangle{ getVertex1 :: !Vertex
+                        , getVertex2 :: !Vertex
+                        , getVertex3 :: !Vertex
+                        } deriving(Show, Eq)
+ 
+
+type Pixel    = Vertex
 type Vec3     = Point
 data Vec2     = Vec2{getVec2X :: Double, getVec2Y :: Double}
 type Position = Point
 
---Projection from the 3D-Space on the Canvas
-projectTriangle :: Triangle -> Triangle
-projectTriangle triangle = Triangle{ getPoint0 = projectPoint' . getPoint0 $ triangle
-									,getPoint1 = projectPoint' . getPoint1 $ triangle
-									,getPoint2 = projectPoint' . getPoint2 $ triangle
-								   }
+
  
 projectPoint' :: Point -> Point
 projectPoint' point = projectPoint 3.4 point
@@ -22,13 +29,20 @@ projectPoint' point = projectPoint 3.4 point
 
 projectPoint :: Double -> Point -> Point
 projectPoint near p = let z = getZ p
-					  in Point{ getX = near * getX p / z
-							   ,getY = near * getY p / z
-							   ,getZ = z
-							}
+                      in Point{ getX = near * getX p / z
+                              , getY = near * getY p / z
+                              , getZ = z
+                              }
 
-projectTriangles :: [Triangle]->[Triangle]
+projectTriangles :: [Triangle] -> [Triangle]
 projectTriangles triangles = map projectTriangle triangles
+  where
+    --Projection from the 3D-Space on the Canvas
+    projectTriangle :: Triangle -> Triangle
+    projectTriangle triangle = Triangle{ getVertex1 = projectPoint' . getVertex1 $ triangle
+                                        ,getVertex2 = projectPoint' . getVertex2 $ triangle
+                                        ,getVertex3 = projectPoint' . getVertex3 $ triangle
+                                        }
 
 cutOfZ :: Vec3->Vec2
 cutOfZ v = Vec2{getVec2X = getX v, getVec2Y = getY v}
@@ -44,9 +58,9 @@ map projectTriangle (x:xs)
 rasteriser :: [Triangle]->[Pixel]-> Position ->[Pixel]
 rasteriser (x:xs) pixel pos = 
 
-							let v0 = perspectiveProject $ getEdge ((getPoint0 x) (getPoint1 x)) pos 
-							      v1 = perspectiveProject $ getEdge ((getPoint1 x) (getPoint2 x)) pos
-							   	  v2 = perspectiveProject $ getEdge ((getPoint2 x) (getPoint0 x)) pos
-							   in rasteriser $ xs pixelPainter ( pixel v0 v1 v2 ) pos
--}							    
+                            let v0 = perspectiveProject $ getEdge ((getPoint0 x) (getPoint1 x)) pos 
+                                  v1 = perspectiveProject $ getEdge ((getPoint1 x) (getPoint2 x)) pos
+                                  v2 = perspectiveProject $ getEdge ((getPoint2 x) (getPoint0 x)) pos
+                               in rasteriser $ xs pixelPainter ( pixel v0 v1 v2 ) pos
+-}                              
  
